3d7954a745f499cb70cc92e96ae967d57801479f
Ankan Mandal <ankanmandal2001@gmail.com>
2025-09-09 12:12:20 +0530

    added changes

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ app.use('/api/forms', formRoutes); // Routes for /api/forms/contact, /api/forms/
 
 
 // --- Start Server ---
-const PORT = process.env.PORT || 5000;
+/*const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
-});
+});*/
+
+module.exports = app
